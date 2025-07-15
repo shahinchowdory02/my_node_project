264f3a9ec97fd751c3adca66f3e34e0958ea2522
shahinchowdory02 <shaheenchowdory80250@gmail.com>
2025-07-15 23:07:48 +0600

    15 07 2025

--- express/app.js
+++ express/app.js
@@ -3,36 +3,44 @@ import { PORT } from "./env.js";
 
 const app = express();
 
-app.get("/", (req, res) => res.send("<h1>Hello world</h1>"));
-
-app.get("/about", (req, res) => res.send("<h1>Hello About Page</h1>"));
-
-app.get("/contact", (req, res) => {
-  return res.send(`<div class="container">
-      <h1>URL Shortener</h1>
-      <from id="shorten-form">
-        <div>
-          <label for="url">Enter URL: </label>
-          <input type="url" name="url" id="url" required />
-        </div>
-
-        <div class="short">
-          <label for="shortCode">Enter shortCode:</label>
-
-          <input type="text" name="shortCode" id="shortCode" required />
-        </div>
-
-        <button class="shorten-btn" type="submit">Shorten</button>
-      </from>
-
-      <h2>Shortened URLs</h2>
-
-      <ul id="shortened-urls"></ul>
-    </div>
-`);
+app.get("/", (req, res) => {
+  // console.log(__dirname);
+  // console.log(__filename);
+  console.log(import.meta.dirname);
+  res.send("Hi");
 });
+
+app.listen(PORT, () => {
+  console.log("Server starting on port 3000");
+});
+
+// app.get("/", (req, res) => res.send("<h1>Hello world</h1>"));
+
+// app.get("/about", (req, res) => res.send("<h1>Hello About Page</h1>"));
+
+// app.get("/contact", (req, res) => {
+//   return res.send(`<div class="container">
+//       <h1>URL Shortener</h1>
+//       <from id="shorten-form">
+//         <div>
+//           <label for="url">Enter URL: </label>
+//           <input type="url" name="url" id="url" required />
+//         </div>
+
+//         <div class="short">
+//           <label for="shortCode">Enter shortCode:</label>
+
+//           <input type="text" name="shortCode" id="shortCode" required />
+//         </div>
+
+//         <button class="shorten-btn" type="submit">Shorten</button>
+//       </from>
+
+//       <h2>Shortened URLs</h2>
+
+//       <ul id="shortened-urls"></ul>
+//     </div>
+// `);
+// });
 
 // const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running at port: ${PORT}`);
-});
