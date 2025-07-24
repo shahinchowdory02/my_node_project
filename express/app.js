c1ba51dd25fd244237c918450625ac23642f7947
shahinchowdory02 <shaheenchowdory80250@gmail.com>
2025-07-25 00:47:47 +0600

    25 07 2025

--- express/app.js
+++ express/app.js
@@ -4,10 +4,22 @@ import path from "path";
 
 const app = express();
 
-app.get("/", (req, res) => {
-  const homePagePath = path.join(import.meta.dirname, "public", "index.html");
-  res.sendFile(homePagePath);
-});
+// app.get("/", (req, res) => {
+//   const homePagePath = path.join(import.meta.dirname, "public", "index.html");
+//   res.sendFile(homePagePath);
+// });
+
+// const response = await fetch("https://jsonplaceholder.typicode.com/todos/1");
+// const json = await response.json();
+// console.log(json);
+
+console.log(import.meta.dirname);
+console.log(import.meta.filename);
+
+const staticPath = path.join(import.meta.dirname, "public");
+console.log(staticPath);
+
+app.use(express.static(staticPath));
 
 app.listen(PORT, () => {
   console.log("Server starting on port 3000");
